ffbf6981454054c1f25316baff159aa62d451da1
Shashank767 <143302681+Shashank23123458867504@users.noreply.github.com>
2024-09-06 11:18:23 +0530

    Create imdb.py

---
+++ imdb.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+df=pd.read_csv('IMDB Dataset.csv')
+X = df['review']
+y = df['sentiment'].apply(lambda x: 1 if x == 'positive' else 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+vectorizer = TfidfVectorizer(stop_words='english', max_features=5000,ngram_range=(1,3))
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+model = LogisticRegression()
+model.fit(X_train_tfidf, y_train)
+y_pred = model.predict(X_test_tfidf)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+print(f'Classification Report:\n{report}')
